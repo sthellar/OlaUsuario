61f4e6fb0dec6305cbe9f875e07e0cf82b7b9771
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-10-29 21:11:22 -0300

    Adicionar a solução do execício Olá Usuário.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Para darmos início, por favor digete o seu nome.....:");
+
+string Nome = Console.ReadLine(); 
+Console.WriteLine("_\n_");
+Console.Write("Olá,");
+Console.Write(Nome);
